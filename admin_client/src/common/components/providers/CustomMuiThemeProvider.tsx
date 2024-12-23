cd7b70333ec9ccb26f55d2ace0d02c7316e7de4f
BlaxBerry <chenjiaxu333@gmail.com>
2024-12-24 06:23:24 +0900

    style(admin_client): modify scrollbar style

--- admin_client/src/common/components/providers/CustomMuiThemeProvider.tsx
+++ admin_client/src/common/components/providers/CustomMuiThemeProvider.tsx
@@ -3,6 +3,7 @@ import { createContext, memo, useEffect, useMemo } from "react";
 
 import MuiCssBaseline from "@mui/material/CssBaseline";
 import {
+  alpha,
   createTheme as createMuiTheme,
   ThemeProvider as MuiThemeProvider,
   type Theme as MuiTheme,
@@ -42,12 +43,35 @@ const CustomMuiThemeProvider: FC<PropsWithChildren> = ({ children }) => {
   // ----------------------------------------------------------------------------------------------------
 
   useEffect(() => {
-    const mainThemePaletteColor = themePaletteColorOptions.palette.primary.main;
+    const primaryColor: string = alpha(themePaletteColorOptions.palette.primary.main, 0.5);
     const style = document.createElement("style");
     style.innerHTML = `
+      /* 设置选中文本的样式 */
       ::selection {
-        background-color: ${mainThemePaletteColor};  /* 设置选中文本的背景色 */
-        color: white;                                /* 设置选中文本的文字颜色 */
+        background-color: ${primaryColor};                              /* 设置选中文本的背景色 */
+        color: white;                                                   /* 设置选中文本的文字颜色 */
+      }
+
+      /* 设置滚动条的样式 */
+      ::-webkit-scrollbar {
+        width: 8px;                                                     /* 设置滚动条的宽度 */
+        height: 8px;                                                    /* 设置滚动条的高度 */
+      }
+      ::-webkit-scrollbar-thumb {
+        background-color: ${primaryColor};                              /* 设置滚动条的颜色 */
+        border-radius: 4px;                                             /* 设置滚动条的圆角 */
+      }
+      ::-webkit-scrollbar-thumb:hover {
+        background-color: darken(${primaryColor}, 10%);                 /* 悬停时加深颜色 */
+      }
+      ::-webkit-scrollbar-track {
+        background-color: rgba(0, 0, 0, 0.1);                         /* 滑轨颜色 */
+        border-radius: 4px;                                             /* 圆角 */
+      }
+      /* Firefox 滚动条样式 */
+      * {
+        scrollbar-color: ${primaryColor} rgba(0, 0, 0, 0.1);          /* 滑块 和 滑轨颜色 */
+        scrollbar-width: thin;                                          /* 滚动条宽度 */
       }
     `;
     document.head.appendChild(style);
